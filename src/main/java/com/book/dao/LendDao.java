20c697d8c83041522ab500f32075171f99f93554
xiaoqi64 <xiaoqi64@outlook.com>
2019-04-22 15:33:25 +0800

    fix lenDao

--- src/main/java/com/book/dao/LendDao.java
+++ src/main/java/com/book/dao/LendDao.java
@@ -1,91 +1,27 @@
 package com.book.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.*;
 import com.book.domain.Lend;
+import java.util.List;
 
-import java.sql.Array;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-@Repository
-public class LendDao {
+@Mapper
+public interface LendDao {
 
-    private JdbcTemplate jdbcTemplate;
-    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+    @Update("UPDATE lend_list SET back_date = #{date} WHERE book_id = #{id} AND back_date is NULL")
+    int bookReturnOne(@Param("date") String date,@Param("id") long bookId);
 
-    @Autowired
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    @Update("UPDATE book_info SET state = 1 WHERE book_id = #{id} ")
+    int bookReturnTwo(@Param("id") long bookId);
 
-    private final static String BOOK_RETURN_SQL_ONE="UPDATE lend_list SET back_date = ? WHERE book_id = ? AND back_date is NULL";
+    @Insert("INSERT INTO lend_list (book_id,reader_id,lend_date) VALUES ( #{bookId} , #{readerId} , #{date} )")
+    int bookLendOne(@Param("bookId") long bookId,@Param("readerId") int readerId,@Param("date") String date);
 
-    private final static String BOOK_RETURN_SQL_TWO="UPDATE book_info SET state = 1 WHERE book_id = ? ";
+    @Update("UPDATE book_info SET state = 0 WHERE book_id = #{id}")
+    int bookLendTwo(@Param("id") long bookId);
 
-    private final static String BOOK_LEND_SQL_ONE="INSERT INTO lend_list (book_id,reader_id,lend_date) VALUES ( ? , ? , ? )";
+    @Select("SELECT * FROM lend_list")
+    List<Lend> lendList();
 
-    private final static String BOOK_LEND_SQL_TWO="UPDATE book_info SET state = 0 WHERE book_id = ? ";
-
-    private final static String LEND_LIST_SQL="SELECT * FROM lend_list";
-
-    private final static String MY_LEND_LIST_SQL="SELECT * FROM lend_list WHERE reader_id = ? ";
-
-    public int bookReturnOne(long bookId){
-        return  jdbcTemplate.update(BOOK_RETURN_SQL_ONE,new Object[]{df.format(new Date()),bookId});
-    }
-    public int bookReturnTwo(long bookId){
-        return jdbcTemplate.update(BOOK_RETURN_SQL_TWO,new Object[]{bookId});
-    }
-    public int bookLendOne(long bookId,int readerId){
-        return  jdbcTemplate.update(BOOK_LEND_SQL_ONE,new Object[]{bookId,readerId,df.format(new Date())});
-    }
-    public int bookLendTwo(long bookId){
-        return  jdbcTemplate.update(BOOK_LEND_SQL_TWO,new Object[]{bookId});
-    }
-
-    public ArrayList<Lend> lendList(){
-        final ArrayList<Lend> list=new ArrayList<Lend>();
-
-        jdbcTemplate.query(LEND_LIST_SQL, new RowCallbackHandler() {
-            public void processRow(ResultSet resultSet) throws SQLException {
-                resultSet.beforeFirst();
-                while (resultSet.next()){
-                    Lend lend=new Lend();
-                    lend.setBackDate(resultSet.getDate("back_date"));
-                    lend.setBookId(resultSet.getLong("book_id"));
-                    lend.setLendDate(resultSet.getDate("lend_date"));
-                    lend.setReaderId(resultSet.getInt("reader_id"));
-                    lend.setSernum(resultSet.getLong("sernum"));
-                    list.add(lend);
-                }
-            }
-        });
-        return list;
-    }
-
-    public ArrayList<Lend> myLendList(int readerId){
-        final ArrayList<Lend> list=new ArrayList<Lend>();
-
-        jdbcTemplate.query(MY_LEND_LIST_SQL, new Object[]{readerId},new RowCallbackHandler() {
-            public void processRow(ResultSet resultSet) throws SQLException {
-                resultSet.beforeFirst();
-                while (resultSet.next()){
-                    Lend lend=new Lend();
-                    lend.setBackDate(resultSet.getDate("back_date"));
-                    lend.setBookId(resultSet.getLong("book_id"));
-                    lend.setLendDate(resultSet.getDate("lend_date"));
-                    lend.setReaderId(resultSet.getInt("reader_id"));
-                    lend.setSernum(resultSet.getLong("sernum"));
-                    list.add(lend);
-                }
-            }
-        });
-        return list;
-
-    }
+    @Select("SELECT * FROM lend_list WHERE reader_id = #{id} ")
+    List<Lend> myLendList(@Param("id") int readerId);
 }
--- src/main/java/com/book/service/LendService.java
+++ src/main/java/com/book/service/LendService.java
@@ -5,23 +5,25 @@ import com.book.domain.Lend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Service
 public class LendService {
     private LendDao lendDao;
-
+    private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
     @Autowired
     public void setLendDao(LendDao lendDao) {
         this.lendDao = lendDao;
     }
 
     public boolean bookReturn(long bookId){
-        return lendDao.bookReturnOne(bookId)>0 && lendDao.bookReturnTwo(bookId)>0;
+        return lendDao.bookReturnOne(df.format(new Date()),bookId)>0 && lendDao.bookReturnTwo(bookId)>0;
     }
 
     public boolean bookLend(long bookId,int readerId){
-        return lendDao.bookLendOne(bookId,readerId)>0 && lendDao.bookLendTwo(bookId)>0;
+        return lendDao.bookLendOne(bookId,readerId,df.format(new Date()))>0 && lendDao.bookLendTwo(bookId)>0;
     }
 
     public ArrayList<Lend> lendList(){
